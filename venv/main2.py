b4de915b6d078e62d3d035b983fd711f23f410b1
Kirill Iizumskii <mr.llirus@gmail.com>
2020-08-17 23:15:59 +0300

    Iterators. Generators. Yield fix

--- venv/main2.py
+++ venv/main2.py
@@ -4,11 +4,10 @@ from hashlib import md5
 def hash_generator(start, end):
     while (start < end):
         with open('countries-links.txt', encoding='utf-8') as file:
-            f_line = file.read()
-            md5_line = md5(f_line.encode()).hexdigest()
-        yield md5_line
-        #yield f_line
-        start += 1
+            for line in file:
+                md5_line = md5(line.encode()).hexdigest()
+                yield md5_line
+                start += 1
 
 for item in hash_generator(1, 250):
     print(item)
